0e38b3ce973564932cded687ce93e9e8f362c8ca
almant12 <dokoalmant123@gmail.com>
2025-01-29 00:10:34 +0100

    update

--- src/handlers/FileImageHandler.ts
+++ src/handlers/FileImageHandler.ts
@@ -17,29 +17,26 @@ export class FileImageHandler extends BaseImageHandler implements ImageHandler {
     
 
     fs.writeFileSync(filePath, buffer);
-    return path.relative(path.join(process.cwd(), 'public'), filePath);
+   // Ensure that the returned path uses forward slashes
+   const relativePath = path.relative(path.join(process.cwd(), 'public'), filePath);
+    
+   // Replace backslashes with forward slashes for compatibility
+   return '/'+relativePath.replace(/\\/g, '/');
   }
 
-    /**
-   * Saves multiple images to the destination path.
-   * @param images An array of File objects to save.
-   * @param destinationPath The path where the images will be saved.
-   * @returns An array of relative file paths for the saved images.
-   */
-    async saveMultiple(images: File | File[], destinationPath?: string): Promise<string[]> {
-      // Normalize input to an array
-      const filesArray = Array.isArray(images) ? images : [images];
-    
-      const savedPaths: string[] = [];
-    
-      for (const image of filesArray) {
-        const savedPath = await this.save(image, destinationPath);
-        savedPaths.push(savedPath);
-      }
-    
-      return savedPaths;
+  async saveMultiple(images: File | File[], destinationPath?: string): Promise<string[]> {
+    // Normalize input to an array
+    const filesArray = Array.isArray(images) ? images : [images];
+  
+    const savedPaths: string[] = [];
+  
+    for (const image of filesArray) {
+      const savedPath = await this.save(image, destinationPath);
+      savedPaths.push(savedPath);
     }
   
+    return savedPaths;
+  }
 
   async update(image: File, oldPath: string, destinationPath?: string): Promise<string> {
     await this.delete(oldPath);
