6ab4f68120b204335a74f2d44389eab9d942fb2f
almant12 <dokoalmant123@gmail.com>
2025-07-11 19:41:35 +0200

    Stop tracking public folder and test.ts

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { ImageHandlers } from "./src/index";
-
-(async () => {
-  const result = await ImageHandlers.urlHandler.save(
-    "https://jpeg.org/images/jpeg-home.jpg",
-    "images"
-  );
-  console.log("Saved to:", result);
-})();
